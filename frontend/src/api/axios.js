9245ea48a1d98829b83088a8b269b0c5f12829ac
Samuel Setarige <setarigesamuel@gmail.com>
2025-07-23 15:40:37 +0300

    Fix API to use Render backend URL for registration

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -5,6 +5,7 @@ const api = axios.create({
     withCredentials: true,
 });
 
+// Optional: token injection
 api.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem('token');
